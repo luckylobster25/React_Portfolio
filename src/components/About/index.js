e4f5915320eb24cb094602c2c6fca8c33c919fd9
Chue Chang <chuechang1996@outlook.com>
2022-03-06 15:39:46 -0600

    added profile picture

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,13 +5,17 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        <img
+        src={require(`../../assets/profile/myprofilepicture-small.JPEG`)}
+        alt='chue profile'
+        className="profile-picture"
+      />
         </div>
         <p>
         Hello, my name is Chue Chang and I am a full-stack Javascript developer. I am a student of the University of Minnesota coding bootcamp. My main programing language is Javascript. I have background in Python as well. 
         </p>
         <p>
-        As of 2022, I am looking forward to any opportunities that come my way. I plan to collabrate with my fellow classsmates to create unique projects and applications. I am very open to many things.  
+        As of 2022, I am looking forward to any opportunities that come my way. I plan to collaborate with my fellow classsmates to create unique projects and applications. I am very open to many things.  
         </p>
       </div>
     </section>
