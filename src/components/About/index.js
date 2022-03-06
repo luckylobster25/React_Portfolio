64b05c4e7efc3e910b6d34c50c378876fdf85c91
Chue Chang <chuechang1996@outlook.com>
2022-03-06 14:36:32 -0600

    add my link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Hello, my name is Chue Chang and I am a full-stack Javascript developer. I am a student of the University of Minnesota coding bootcamp. My main programing is Javascript. I have background in Python as well. 
+        Hello, my name is Chue Chang and I am a full-stack Javascript developer. I am a student of the University of Minnesota coding bootcamp. My main programing language is Javascript. I have background in Python as well. 
         </p>
         <p>
         As of 2022, I am looking forward to any opportunities that come my way. I plan to collabrate with my fellow classsmates to create unique projects and applications. I am very open to many things.  
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,15 +6,15 @@ function Footer() {
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/luckylobster25"
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/chue-chang-674b71223/"
     },
     {
       name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
+      link: "https://stackoverflow.com/users/15435961/chue-chang"
     }
   ]
 
