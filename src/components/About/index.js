7d43b9fe48004ce9d201bcb966ca7359ee5766c8
Chue Chang <chuechang1996@outlook.com>
2022-03-06 14:28:47 -0600

    update about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,10 +8,10 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
+        Hello, my name is Chue Chang and I am a full-stack Javascript developer. I am a student of the University of Minnesota coding bootcamp. My main programing is Javascript. I have background in Python as well. 
         </p>
         <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+        As of 2022, I am looking forward to any opportunities that come my way. I plan to collabrate with my fellow classsmates to create unique projects and applications. I am very open to many things.  
         </p>
       </div>
     </section>
